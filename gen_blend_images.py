584bb450cca4a8208e81404a1d7d61c7b198a716
Mayur Mudigonda <mayur.mudigonda@gmail.com>
2014-10-31 17:53:12 -0700

    Script to make blend files is now only in python. Started working on motor+haptic signal. Need pain meds for my neck

---
+++ gen_blend_images.py
@@ -0,0 +1,27 @@
+import bpy
+
+pos_range = 3
+fov=50
+scene = bpy.context.scene
+#scene.render.image_settings.file_fomat = 'jpg'
+scene.render.resolution_x = 512
+scene.render.resolution_y = 512
+
+scene.camera.data.angle = fov*(pi/180.0)
+
+
+
+for object in bpy.data.objects:
+    if object.name != 'Lamp' and object.name!= 'Camera':
+        object.select = True
+bpy.ops.object.delete()
+bpy.ops.mesh.primitive_ico_sphere_add()
+
+for xx in range(-pos_range,pos_range+1):
+    for yy in range(-pos_range,pos_range+1):
+        for zz in range(-pos_range,pos_range+1):
+            bpy.data.objects['Icosphere'].location = (xx,yy,zz)
+            #path = '/Users/grndfthrprdx/Development/bpy_data/sphere_'+str(xx)+'_'+str(yy)+'_'+str(zz)+'.jpg'
+            path = 'sphere_'+str(xx)+'_'+str(yy)+'_'+str(zz)+'.jpg'
+            bpy.data.scenes['Scene'].render.filepath = path
+            bpy.ops.render.render(write_still=True)
---
+++ gen_mot_haptic.py
@@ -0,0 +1,6 @@
+import numpy as np
+import os 
+import sys
+
+#First read the image file name
+for file in 
